9253ca0302e2e5afcaef0483f127abf19b7e6aec
Mithra <kit28.24bcb032@gmail.com>
2025-12-17 16:22:10 +0530

    updated getters and setters and created userRepository.java file

---
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.repository;
+import java.lang.foreign.Linker.Option;
+
+import org.springframework. data.jpa.repository.JpaRepository;
+import com.ecommerce.entity.User;
+public class UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    @Query("SELECT u FROM USER u WHERE u.username = :username OR u.email = :email")
+    Optional<User> findByUsernameOrEmail(@Param("username") String username, @Param("email") String email);
+}
